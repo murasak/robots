8f07cce4d4da935ffe0a90b902b9a6714a1cd3eb
monica <monica@hesper.io>
2019-02-02 13:48:19 -0500

    Updates

---
+++ precache-manifest.6c1b8f7a0215fccff082e621e4c6c78d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/murasak/robots/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a006f403308444a9ec36",
+    "url": "/murasak/robots/static/js/runtime~main.a006f403.js"
+  },
+  {
+    "revision": "bc89ecd3fab7c63a52f7",
+    "url": "/murasak/robots/static/js/main.bc89ecd3.chunk.js"
+  },
+  {
+    "revision": "72442ade8c56b3798823",
+    "url": "/murasak/robots/static/js/1.72442ade.chunk.js"
+  },
+  {
+    "revision": "bc89ecd3fab7c63a52f7",
+    "url": "/murasak/robots/static/css/main.83155dfb.chunk.css"
+  },
+  {
+    "revision": "72442ade8c56b3798823",
+    "url": "/murasak/robots/static/css/1.64bbcf54.chunk.css"
+  },
+  {
+    "revision": "35247a74c6fb6c6fb31e2bf13b3f03b8",
+    "url": "/murasak/robots/index.html"
+  }
+];
